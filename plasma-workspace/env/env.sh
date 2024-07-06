b8c540fdecc3f6e09ea7973811248c662b45526a
TD-Sky <three-dim-sky@foxmail.com>
2024-07-06 18:12:27 +0800

    2024-07-06 18:12:27+08:00

--- plasma-workspace/env/env.sh
+++ plasma-workspace/env/env.sh
@@ -2,8 +2,8 @@ export LANG=zh_CN.UTF-8
 export LANGUAGE=zh_CN:en_US
 
 # fcitx
+# NOTE: https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland#KDE_Plasma
 export XMODIFIERS='@im=fcitx'
-export QT_IM_MODULE='fcitx'
 export SDL_IM_MODULE='fcitx'
 # scripts
 export PATH="${HOME}/.local/bin:${PATH}"
