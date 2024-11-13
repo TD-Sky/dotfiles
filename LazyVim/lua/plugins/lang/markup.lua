9b75b4cda95abcac8582e0c3468d094275cf6b61
TD-Sky <three-dim-sky@foxmail.com>
2024-11-13 23:03:13 +0800

    2024-11-13 23:03:13+08:00

--- LazyVim/lua/plugins/lang/markup.lua
+++ LazyVim/lua/plugins/lang/markup.lua
@@ -22,4 +22,36 @@ return {
             vim.g.vimtex_compiler_latexmk = { continuous = 0, callback = 0 }
         end,
     },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+            code = {
+                sign = false,
+                width = "block",
+                right_pad = 1,
+            },
+            heading = {
+                sign = false,
+                icons = {},
+            },
+        },
+        ft = { "markdown", "norg", "rmd", "org" },
+        config = function(_, opts)
+            require("render-markdown").setup(opts)
+            Snacks.toggle({
+                name = "Render Markdown",
+                get = function()
+                    return require("render-markdown.state").enabled
+                end,
+                set = function(enabled)
+                    local m = require("render-markdown")
+                    if enabled then
+                        m.enable()
+                    else
+                        m.disable()
+                    end
+                end,
+            }):map("<leader>um")
+        end,
+    },
 }
