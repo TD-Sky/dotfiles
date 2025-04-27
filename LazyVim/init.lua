2615bec6541ad1be25accc8a6380566c630ec739
TD-Sky <three-dim-sky@foxmail.com>
2025-04-27 14:37:05 +0800

    2025-04-27 14:37:05+08:00

--- LazyVim/init.lua
+++ LazyVim/init.lua
@@ -3,8 +3,6 @@ require("config.filetypes")
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.g.neovide then
-    require("config.neovide")
-end
+require("config.neovide")
 
 vim.cmd.highlight("CursorWord guifg=#FF6868")
