086edd954b458a9983855a49264749e4e23ab9b6
TD-Sky <three-dim-sky@foxmail.com>
2025-06-03 17:05:03 +0800

    2025-06-03 17:05:03+08:00

--- LazyVim/lua/plugins/nav.lua
+++ LazyVim/lua/plugins/nav.lua
@@ -120,6 +120,23 @@ return {
                     })
                 end,
                 mode = { "n", "v" },
+                desc = "Search and Replace AST in current file",
+            },
+            {
+                "<leader>sA",
+                function()
+                    local grug = require("grug-far")
+
+                    local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+                    grug.open({
+                        engine = "astgrep",
+                        transient = true,
+                        prefills = {
+                            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+                        },
+                    })
+                end,
+                mode = { "n", "v" },
                 desc = "Search and Replace AST",
             },
         },
