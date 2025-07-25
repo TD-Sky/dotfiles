70e3d8def9707ffedbf14c77f15ad892dca3a949
TD-Sky <three-dim-sky@foxmail.com>
2025-07-25 10:51:26 +0800

    2025-07-25 10:51:26+08:00

--- LazyVim/lua/plugins/misc.lua
+++ LazyVim/lua/plugins/misc.lua
@@ -37,34 +37,6 @@ return {
         "folke/snacks.nvim",
         keys = {
             {
-                "<leader>ff",
-                function()
-                    Snacks.picker.files()
-                end,
-                desc = "Find Files (cwd)",
-            },
-            {
-                "<leader>fF",
-                function()
-                    Snacks.picker.files({ cwd = require("lazyvim.util").root.get() })
-                end,
-                desc = "Find Files (Root Dir)",
-            },
-            {
-                "<leader>sg",
-                function()
-                    Snacks.picker.grep()
-                end,
-                desc = "Grep (cwd)",
-            },
-            {
-                "<leader>sG",
-                function()
-                    Snacks.picker.grep({ cwd = require("lazyvim.util").root.get() })
-                end,
-                desc = "Grep (Root Dir)",
-            },
-            {
                 "<leader>ft",
                 function()
                     Snacks.terminal()
