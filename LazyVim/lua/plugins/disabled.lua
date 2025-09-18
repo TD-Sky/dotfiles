8c77ae8eb354166dfcca2bcd979a330b6b1ed798
TD-Sky <three-dim-sky@foxmail.com>
2025-09-18 19:43:51 +0800

    2025-09-18 19:43:51+08:00

--- LazyVim/lua/plugins/disabled.lua
+++ LazyVim/lua/plugins/disabled.lua
@@ -2,8 +2,8 @@ return {
     -- 禁用会导致预配置加载异常
     -- { "folke/tokyonight.nvim", enabled = false },
 
-    { "williamboman/mason-lspconfig.nvim", enabled = false },
-    { "williamboman/mason.nvim", enabled = false },
+    { "mason-org/mason-lspconfig.nvim", enabled = false },
+    { "mason-org/mason.nvim", enabled = false },
     { "catppuccin/nvim", name = "catppuccin", enabled = false },
     { "mfussenegger/nvim-lint", enabled = false },
     { "stevearc/conform.nvim", enabled = false },
