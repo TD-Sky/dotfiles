ab3bc5c8aa9d736bb1c0cc074dfc8ad9fdf75e60
TD-Sky <three-dim-sky@foxmail.com>
2025-10-17 00:43:02 +0800

    2025-10-17 00:43:02+08:00

--- LazyVim/lua/plugins/snippets/rust.lua
+++ LazyVim/lua/plugins/snippets/rust.lua
@@ -1,20 +1,7 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
 local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
 local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local events = require("luasnip.util.events")
-local ai = require("luasnip.nodes.absolute_indexer")
-local extras = require("luasnip.extras")
-local fmt = extras.fmt
-local m = extras.m
-local l = extras.l
-local postfix = require("luasnip.extras.postfix").postfix
 
 return {
     s("lf", {
